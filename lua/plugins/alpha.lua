cb36ddd191f70c6d60df473ef2386c60de4ac857
Dave Jordan <le_dangereux@hotmail.co.uk>
2022-02-10 21:10:50 +0000

    Replace alpha dashboard with DaveVim splash

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -7,30 +7,83 @@ M.config = function ()
     return
   end
 
-  local dashboard_status_ok, dashboard = pcall(require, "alpha.themes.dashboard")
-  if not dashboard_status_ok then
-    print "Failed to load dashboard theme for alpha"
-    return
-  end
-
-  dashboard.section.header.val = {
+  local header = {
+    type = "text",
+    val = {
       "██████╗░░█████╗░██╗░░░██╗███████╗██╗░░░██╗██╗███╗░░░███╗",
       "██╔══██╗██╔══██╗██║░░░██║██╔════╝██║░░░██║██║████╗░████║",
       "██║░░██║███████║╚██╗░██╔╝█████╗░░╚██╗░██╔╝██║██╔████╔██║",
       "██║░░██║██╔══██║░╚████╔╝░██╔══╝░░░╚████╔╝░██║██║╚██╔╝██║",
       "██████╔╝██║░░██║░░╚██╔╝░░███████╗░░╚██╔╝░░██║██║░╚═╝░██║",
-      "╚═════╝░╚═╝░░╚═╝░░░╚═╝░░░╚══════╝░░░╚═╝░░░╚═╝╚═╝░░░░░╚═╝"
+      "╚═════╝░╚═╝░░╚═╝░░░╚═╝░░░╚══════╝░░░╚═╝░░░╚═╝╚═╝░░░░░╚═╝",
+    },
+    opts = {
+      position = "center",
+      hl = "AlphaHeader"
+    }
   }
 
-  dashboard.section.buttons.val = {
-    dashboard.button( "e", "  > New file" , ":ene <BAR> startinsert <CR>"),
-    dashboard.button( "f", "  > Find file", ":cd $HOME/ | Telescope find_files<CR>"),
-    dashboard.button( "r", "  > Recent"   , ":Telescope oldfiles<CR>"),
-    dashboard.button( "s", "  > Settings" , ":e $MYVIMRC | :cd %:p:h | split . | wincmd k | pwd<CR>"),
-    dashboard.button( "q", "  > Quit NVIM", ":qa<CR>"),
+  local function button(sc, txt, keybind)
+    local sc_ = sc:gsub("%s", ""):gsub("SPC", "<leader>")
+
+    local opts = {
+        position = "center",
+        text = txt,
+        shortcut = sc,
+        cursor = 5,
+        width = 24,
+        align_shortcut = "right",
+        hl_shortcut = "AlphaButtons",
+        hl = "AlphaButtons",
+    }
+    if keybind then
+        opts.keymap = {"n", sc_, keybind, {noremap = true, silent = true}}
+    end
+
+    return {
+        type = "button",
+        val = txt,
+        on_press = function()
+            local key = vim.api.nvim_replace_termcodes(sc_, true, false, true)
+            vim.api.nvim_feedkeys(key, "normal", false)
+        end,
+        opts = opts,
+    }
+  end
+
+  local buttons = {
+    type = "group",
+    val = {
+    button( "LDR y", "   Explore" , ":Telescope file_browser<CR>"),
+    button( "LDR h", "   Recents" , ":Telescope oldfiles<CR>"),
+    button( "LDR /", "   Ripgrep" , ":Telescope live_grep<CR>"),
+    -- button( "LDR b", "   Buffers" , ":Telescope buffers<CR>"),
+    -- button( "MRK V", "   Options" , ":execute 'normal! `V'<CR>"),
+    -- button( "MRK P", "   Plugins" , ":execute 'normal! `P'<CR>"),
+    },
+    opts = {
+        spacing = 1,
+    }
   }
 
-  alpha.setup(dashboard.opts)
+  local opts = {
+    layout = {
+      { type = "padding", val = 5 },
+      header,
+      { type = "padding", val = 3 },
+      -- section.heading,
+      -- section.plugin_count,
+      -- { type = "padding", val = 1 },
+      buttons,
+      -- { type = "padding", val = 1 },
+      -- section.footer
+    },
+    opts = {
+      margin = 5
+    }
+  }
+
+  alpha.setup(opts)
 end
 
 return M
