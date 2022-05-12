abc050c43edfcf1016404a8d996204519d64ba3f
Dave Jordan <le_dangereux@hotmail.co.uk>
2022-05-12 09:03:10 +0100

    Shrink toggleterm pane

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -5,7 +5,7 @@ local settings = {
     if term.direction == "horizontal" then
       return 12
     elseif term.direction == "vertical" then
-      return vim.o.columns * 0.5
+      return vim.o.columns * 0.33
     end
   end,
   open_mapping = "<Leader>#",
