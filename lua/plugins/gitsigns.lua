446c42aad9d51be01f80e27f26c67a28c72b25a3
Dave Jordan <le_dangereux@hotmail.co.uk>
2022-12-09 19:20:39 +0000

    Add untracked file highlighting to gitsigns

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -3,10 +3,12 @@ local M = {}
 local settings = {
   signs = {
     add = { hl = "GitSignsAdd", text = "┃", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn", },
-    change = { hl = "GitSignsChange", text = "┃", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn", },
-    delete = { hl = "GitSignsDelete", text = "契", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn", },
-    topdelete = { hl = "GitSignsDelete", text = "契", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn", },
-    changedelete = { hl = "GitSignsChange", text = "┃", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn", },
+    change = { hl = "GitSignsChange", text = "┃", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
+    delete = { hl = "GitSignsDelete", text = "契", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
+    topdelete = { hl = "GitSignsDelete", text = "契", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
+    changedelete = { hl = "GitSignsChange", text = "┃", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
+    -- TODO: Different symbol for untracked?
+    untracked = { hl = "GitSignsUntracked", text = "┃", numhl = "GitSignsUntrackedNr", linehl = "GitSignsUntrackedLn" },
   },
   current_line_blame = true,
   current_line_blame_formatter = "<author>, <author_time:%d-%m-%Y> - <summary>",
@@ -23,7 +25,7 @@ M.config = function()
   end
 
   gitsigns.setup(settings)
-  print "Settings applied"
+  print("Settings applied")
 end
 
 return M
