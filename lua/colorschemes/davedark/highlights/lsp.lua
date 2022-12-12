34aa929bc745413eed5f05df17b2cf4276215806
Dave Jordan <le_dangereux@hotmail.co.uk>
2022-12-12 20:36:19 +0000

    Remove unused import from lsp highlights

--- lua/colorschemes/davedark/highlights/lsp.lua
+++ lua/colorschemes/davedark/highlights/lsp.lua
@@ -1,5 +1,4 @@
 local c = require("colorschemes.davedark.palette")
--- local config = require("colorschemes.davedark.config")
 
 return {
   ---------------------------------------------------------
