4d47bea667dcb5a241b3c091eda5a96321da5a79
Dave Jordan <le_dangereux@hotmail.co.uk>
2022-12-17 10:09:51 +0000

    Clean up options and update tabbing / search

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -1,67 +1,77 @@
 local options = {
   -- tabbing
   tabstop = 2, -- insert this many spaces for <Tab>
-  shiftwidth = 0, -- number of steps for (auto)indent, uses tabstop when 0
   expandtab = true, -- convert tabs to spaces
+  smarttab = true, -- delete spaces like they're tabs
+  shiftwidth = 0, -- number of steps for (auto)indent, uses tabstop when 0
 
-  -- word wrap
+  -- word wrap and auto-new-line
   wrap = false,
-  linebreak = true,
-  breakindent = true,
-  showbreak = "  ",
+  linebreak = true, -- prevents wrapping in the middle of a word
+  textwidth = 79,
 
-  -- line breaking
-  paste = false,
+  -- cursor behaviour
+  whichwrap = "b,s,<,>,[,],h,l",
 
   -- indentation
-  -- autoindent = true,
+  autoindent = false,
   -- smartindent = true,
 
-  -- unsorted
-  number = true, -- line numbers
-  relativenumber = true, -- useful for jumping but I like to refer to line numbers
-  mouse = "a", -- full mouse functionality
-  timeoutlen = 200,
+  -- line numbers
+  number = true,
+  relativenumber = true,
+  numberwidth = 3,
+
+  -- search
+  ignorecase = true, -- ignore case while searching
+  smartcase = true, -- resume search case sensitivity if term includes capitals
+  hlsearch = false, -- get rid of annoying search highlight
+  incsearch = true, -- enable
+
+  -- ui
   termguicolors = true, -- enables 24-bit colour
   showtabline = 2, -- always show tabs at top of screen
   cursorline = false, -- gets confusing when cancelling out of Visual mode
+  cmdheight = 2, -- helps avoid 'Press Enter' messages when working in a narrow window
+  showmode = false, -- unneeded with feline statusline
+  laststatus = 3, -- wide statusline for current buffer
   scrolloff = 8, -- minimal number of lines to keep above / below cursor
   sidescrolloff = 8, -- minimal number of lines to keep left / right of cursor
+
+  -- columns
+  signcolumn = "yes",
+  foldcolumn = "2",
+
+  -- windows
   splitbelow = true,
   splitright = true,
-  numberwidth = 3,
-  signcolumn = "yes",
-  ignorecase = true, -- ignore case while searching
-  smartcase = true, -- resume search case sensitivity if term includes capitals
+
+  -- popup menu
   pumheight = 10,
+  completeopt = "menu,menuone,noselect",
+
+  -- unsorted
+  mouse = "a", -- full mouse functionality
+  timeoutlen = 200,
   fileencoding = "utf-8",
   hidden = true, -- the default, but insurance to prevent terminals being abandoned
-  showmode = false,
-  foldcolumn = "2",
-  cmdheight = 2, -- helps avoid 'Press Enter' messages when working in a narrow window
   clipboard = "unnamedplus", -- yank to system clipboard via xcopy
-  laststatus = 3,
-  backspace = "indent,eol,start",
-  completeopt = "menu,menuone,noselect",
 }
 
 -- Set colour scheme
 vim.cmd("colorscheme davedark")
 
--- Gets rid of the ~ indicating non-text / end of buffer
-vim.cmd("set fillchars+=eob:\\ ")
-
--- Enable cursor to wrap across lines
-vim.opt.whichwrap:append("<,>,[,],h,l")
-
 -- Treat "-" as part of a word
 vim.opt.iskeyword:append("-")
 
--- Don't give ins-completion-menu messages (whatever they are?)
-vim.opt.shortmess:append("c")
+-- Gets rid of the ~ indicating non-text / end of buffer
+vim.cmd("set fillchars+=eob:\\ ")
 
 -- Change fold symbols
 vim.opt.fillchars:append("foldopen:🞃, foldsep:│, foldclose:🞂")
+
+-- Don't give ins-completion-menu messages
+vim.opt.shortmess:append("c")
 
 -- Make the cursor blink to visually indicate whether the window is active
 vim.cmd([[
