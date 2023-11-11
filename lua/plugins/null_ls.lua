e1478961afd25d44d1117af1f83846c5eae7cbf8
David Jordan <dave@Dangerbook-Pro.broadband>
2023-11-11 18:46:23 +0000

    Clarify null_ls config

--- lua/plugins/null_ls.lua
+++ lua/plugins/null_ls.lua
@@ -10,67 +10,74 @@ return {
     local formatting = null_ls.builtins.formatting
     local diagnostics = null_ls.builtins.diagnostics
 
-    local lsp_formatting = function(bufnr)
-      if config.format_on_save then
-        vim.lsp.buf.format({
-          timeout_ms = 40000,
-          filter = function(client)
-            return client.name == "null-ls"
-          end,
-          bufnr = bufnr,
-        })
-      end
+    local format_on_save = function(bufnr)
+      vim.lsp.buf.format({
+        timeout_ms = 40000,
+        filter = function(client)
+          return client.name == "null-ls"
+        end,
+        bufnr = bufnr,
+      })
     end
 
     local lsp_formatting_augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
+    local on_attach = function(client, bufnr)
+      if client.supports_method("textDocument/formatting") then
+        vim.api.nvim_clear_autocmds({
+          group = lsp_formatting_augroup,
+          buffer = bufnr,
+        })
+        vim.api.nvim_create_autocmd("BufWritePre", {
+          group = lsp_formatting_augroup,
+          buffer = bufnr,
+          callback = function()
+            if config.format_on_save then
+              format_on_save(bufnr)
+            end
+          end,
+        })
+      end
+    end
+
+    local sources = {
+      code_actions.eslint,
+      code_actions.gitsigns,
+
+      completion.luasnip.with({
+        filetypes = { "lua" },
+      }),
+
+      diagnostics.mypy,
+      diagnostics.ruff,
+
+      formatting.stylua,
+      formatting.eslint_d,
+      formatting.prettierd.with({
+        disabled_filetypes = { "markdown", "vimwiki" },
+      }),
+      formatting.ruff,
+      formatting.stylelint,
+      formatting.autopep8,
+      formatting.clang_format,
+
+      -- diagnostics.twigcs,
+      diagnostics.stylelint.with({
+        extra_args = function()
+          if config.use_stylelint_default_config then
+            return { "--config=" .. config.stylelint_default_config_location }
+          end
+        end,
+      }),
+
+      require("typescript.extensions.null-ls.code-actions"),
+    }
+
     local settings = {
       -- Format on save
       -- TODO: Figure out a directory format shortcut
-      on_attach = function(client, bufnr)
-        if client.supports_method("textDocument/formatting") then
-          vim.api.nvim_clear_autocmds({ group = lsp_formatting_augroup, buffer = bufnr })
-          vim.api.nvim_create_autocmd("BufWritePre", {
-            group = lsp_formatting_augroup,
-            buffer = bufnr,
-            callback = function()
-              lsp_formatting(bufnr)
-            end,
-          })
-        end
-      end,
-      sources = {
-        code_actions.eslint,
-        code_actions.gitsigns,
-
-        completion.luasnip.with({
-          filetypes = { "lua" },
-        }),
-
-        diagnostics.mypy,
-        diagnostics.ruff,
-
-        formatting.stylua,
-        formatting.eslint_d,
-        formatting.prettierd.with({
-          disabled_filetypes = { "markdown", "vimwiki" },
-        }),
-        formatting.ruff,
-        formatting.stylelint,
-        formatting.autopep8,
-        formatting.clang_format,
-
-        -- diagnostics.twigcs,
-        diagnostics.stylelint.with({
-          extra_args = function()
-            if config.use_stylelint_default_config then
-              return { "--config=" .. config.stylelint_default_config_location }
-            end
-          end,
-        }),
-
-        require("typescript.extensions.null-ls.code-actions"),
-      },
+      on_attach = on_attach,
+      sources = sources,
     }
 
     null_ls.setup(settings)
