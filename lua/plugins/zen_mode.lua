774da0ce6a5a4278e6efc306e3bc70e0fe061a62
David Jordan <le_dangereux@hotmail.co.uk>
2024-05-03 10:50:01 +0100

    Adjust indentation and increase width setting in zen_mode.lua

--- lua/plugins/zen_mode.lua
+++ lua/plugins/zen_mode.lua
@@ -1,39 +1,39 @@
 return {
-  "folke/zen-mode.nvim",
-  config = function()
-    local c = require("colorschemes.davedark.themes.default")
-    local config = require("user_config")
+    "folke/zen-mode.nvim",
+    config = function()
+        local c = require("colorschemes.davedark.themes.default")
+        local config = require("user_config")
 
-    local settings = {
-      window = {
-        height = 0.9,
-        width = 100,
-        options = {
-          signcolumn = "no",
-          number = false,
-          relativenumber = false,
-          list = false,
-        },
-      },
-      plugins = {
-        options = {
-          showcmd = false,
-          laststatus = 0,
-        },
-        gitsigns = { enabled = true },
-        tmux = { enabled = false },
-      },
-      -- ZenMode seems to use the popup menu background, so this is a workaround
-      on_open = function()
-        local bg = config.transparent_background and c.none or c.bg
-        vim.cmd("highlight Pmenu guibg=" .. bg)
-      end,
-      on_close = function()
-        local pmenu_bg = config.transparent_background and c.none or c.pmenu_bg
-        vim.cmd("highlight Pmenu guibg=" .. (pmenu_bg or "none"))
-      end,
-    }
+        local settings = {
+            window = {
+                height = 0.9,
+                width = 120,
+                options = {
+                    signcolumn = "no",
+                    number = false,
+                    relativenumber = false,
+                    list = false,
+                },
+            },
+            plugins = {
+                options = {
+                    showcmd = false,
+                    laststatus = 0,
+                },
+                gitsigns = { enabled = true },
+                tmux = { enabled = false },
+            },
+            -- ZenMode seems to use the popup menu background, so this is a workaround
+            on_open = function()
+                local bg = config.transparent_background and c.none or c.bg
+                vim.cmd("highlight Pmenu guibg=" .. bg)
+            end,
+            on_close = function()
+                local pmenu_bg = config.transparent_background and c.none or c.pmenu_bg
+                vim.cmd("highlight Pmenu guibg=" .. (pmenu_bg or "none"))
+            end,
+        }
 
-    require("zen-mode").setup(settings)
-  end,
+        require("zen-mode").setup(settings)
+    end,
 }
